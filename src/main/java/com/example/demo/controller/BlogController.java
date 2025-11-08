5db3fd4fb0c275aa6511fff2d256b471213819db
Stella-1225 <myportfolio1225@gmail.com>
2025-11-08 18:54:59 +0900

    7주차 게시판 수정 및 추가 기능 완료

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import com.example.demo.model.domain.Board;
 
 @Controller // 컨트롤러 어노테이션 명시
 public class BlogController{
@@ -34,11 +35,30 @@ public class BlogController{
     //     return "article_list";
     // }
 
-    @GetMapping("/article_list") // 게시판 링크 지정
-    public String article_list(Model model) {
-        List<Article> list = blogService.findAll(); // 게시판 리스트
-        model.addAttribute("articles", list); // 모델에 추가
-        return "article_list"; // .HTML 연결
+    // @GetMapping("/article_list") // 게시판 링크 지정
+    // public String article_list(Model model) {
+    //     List<Article> list = blogService.findAll(); // 게시판 리스트
+    //     model.addAttribute("articles", list); // 모델에 추가
+    //     return "article_list"; // .HTML 연결
+    // }
+
+    @GetMapping("/board_list") // 새로운 게시판 링크 지정
+    public String board_list(Model model) {
+        List<Board> list = blogService.findAll(); // 게시판 전체 리스트, 기존 Article에서 Board로 변경됨
+        model.addAttribute("boards", list); // 모델에 추가
+        return "board_list"; // .HTML 연결
+    }
+
+    @GetMapping("/board_view/{id}") // 게시판 링크 지정
+    public String board_view(Model model, @PathVariable Long id) {
+        Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
+        if (list.isPresent()) {
+            model.addAttribute("boards", list.get()); // 존재할 경우 실제 Board 객체를 모델에 추가
+            } else {
+                // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+                return "/error_page/article_error"; // 오류 처리 페이지로 연결
+            }
+        return "board_view"; // .HTML 연결
     }
 
     @PostMapping("articles") // post 요청
@@ -59,19 +79,37 @@ public class BlogController{
     //     return "article_edit"; // .HTML 연결
     // }
 
-    @GetMapping("/article_edit/{id}")
-    public String article_edit(Model model, @PathVariable Long id) {
-        Article article = blogService.findById(id)
-                                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
-        model.addAttribute("article", article);
-        return "article_edit";
+    // @GetMapping("/article_edit/{id}")
+    // public String article_edit(Model model, @PathVariable Long id) {
+    //     Article article = blogService.findById(id)
+    //                              .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+    //     model.addAttribute("article", article);
+    //     return "article_edit";
+    // }
+
+    @GetMapping("/board_edit/{id}") // 게시판 링크 지정
+    public String board_edit(Model model, @PathVariable Long id) {
+    Optional<Board> list = blogService.findById(id); // 선택한 게시판 글
+        if (list.isPresent()) {
+            model.addAttribute("board", list.get());
+        } else {
+                // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
+                //return "/error_page/article_error"; //오류 처리 페이지로 연결(이름 수정됨)
+            }
+        return "board_edit"; // .HTML 연결
     }
 
-    @PutMapping("/api/article_edit/{id}")
+    @PutMapping("/api/board_edit/{id}")
     public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
-        blogService.update(id, request);
-        return "redirect:/article_list"; // 글 수정 이후 .html 연결
+         blogService.update(id, request);
+         return "redirect:/board_list"; // 글 수정 이후 .html 연결
     }
+
+    // @PutMapping("/api/article_edit/{id}")
+    // public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
+    //     blogService.update(id, request);
+    //     return "redirect:/article_list"; // 글 수정 이후 .html 연결
+    // }
 
     @DeleteMapping("/api/article_delete/{id}")
     public String deleteArticle(@PathVariable Long id) {
